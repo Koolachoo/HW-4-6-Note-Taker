71c6e4e265986c4ce07a6575f4f341d06d4d4469
Justin Taylor <kiradez211@gmail.com>
2020-04-10 19:06:02 -0400

    Further installation, test file added

--- Develop/server.js
+++ Develop/server.js
@@ -19,7 +19,7 @@ app.get('/api/notes', function(req, res) {
 app.post('/api/notes', function (req, res){
     var newNote = req.body;
     notesArr.push(req.body);
-    fs.writeFile(__dirname + '/db/db.json', JSON.stringify(notesArr), function(notesArr){
+    fs.writeFile(__dirname + '/db/db.json', JSON.stringify(notesArr), function(err){
         res.json(newNote);
     })
 });
@@ -27,11 +27,19 @@ app.post('/api/notes', function (req, res){
 app.delete('/api/notes/:id', function(req, res){
     var id = req.params.id;
     var note = notesArr[id];
-    notesArr.slice(id, id + 1);
-    console.log('')
+    notesArr = notesArr.splice(parseInt(id));
+    console.log('notesArr', notesArr);
+    fs.writeFile(__dirname + '/db/db.json', JSON.stringify(notesArr), function(err){
+        res.json(note);
+    if(err) throw err;
     res.json(note);
+})
 })
 
 app.listen(PORT, function(){
+    fs.readFile(__dirname + '/db/db.json', function(err, data){
+        if(err) throw err;
+        notesArr = JSON.parse(data);
+    })
     console.log('listening');
 })
