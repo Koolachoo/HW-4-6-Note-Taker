8c21d360eaae186c2f10a6392aa10b6bc5d4e491
Justin Taylor <kiradez211@gmail.com>
2020-04-12 04:28:49 -0400

    Additional updates to app.get

--- Develop/server.js
+++ Develop/server.js
@@ -1,45 +1,52 @@
-var express = require('express');
-var fs = require('fs');
-var app = express();
+var express = require("express");
+var fs = require("fs");
 
 var notesArr = [];
+
+var app = express();
 
 var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/api/notes', function(req, res) {
-    fs.readFile(__dirname + 'db/db.json', function(err,data){
-        if(err) throw err;
-        res.json(data);
-    });
+app.get("/api/notes", function(req, res) {
+    res.json(notesArr);
+    res.sendFile(path.join(__dirname, "public/notes.html"))
 });
 
-app.post('/api/notes', function (req, res){
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.post("/api/notes", function(req, res) {
     var newNote = req.body;
-    notesArr.push(req.body);
-    fs.writeFile(__dirname + '/db/db.json', JSON.stringify(notesArr), function(err){
+    notesArr.push(newNote);
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notesArr), function(err) {
         res.json(newNote);
     })
 });
 
-app.delete('/api/notes/:id', function(req, res){
+app.delete("/api/notes/:id", function(req, res) {
     var id = req.params.id;
     var note = notesArr[id];
     notesArr = notesArr.splice(parseInt(id));
-    console.log('notesArr', notesArr);
-    fs.writeFile(__dirname + '/db/db.json', JSON.stringify(notesArr), function(err){
+    console.log("notesArr", notesArr);
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notesArr), function(err) {
+        if(err) throw err;
         res.json(note);
-    if(err) throw err;
-    res.json(note);
-})
-})
+    }) 
+});
 
-app.listen(PORT, function(){
-    fs.readFile(__dirname + '/db/db.json', function(err, data){
+app.listen(PORT, function() {
+    fs.readFile(__dirname + "/db/db.json", function(err, data) {
         if(err) throw err;
         notesArr = JSON.parse(data);
     })
-    console.log('listening');
+    console.log("listening");
 })
