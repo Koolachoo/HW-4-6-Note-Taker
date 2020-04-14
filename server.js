e2c17167aae6c73e94854bfff541dfc1186e4f03
Justin Taylor <kiradez211@gmail.com>
2020-04-14 01:48:08 -0400

    regular

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.use(express.static("public"));
 
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
 });
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
 });
 
 app.get("/api/notes", function (req, res) {
@@ -41,7 +41,7 @@ app.post("/api/notes", function (req, res) {
 });
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
 });
 
 app.delete("/api/notes/:id", function (req, res) {
